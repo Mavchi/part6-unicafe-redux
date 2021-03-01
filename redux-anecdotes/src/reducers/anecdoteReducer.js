6a4adadacf9b25e487c4155b3b5874b8581064f9
Mavchi <aleksi.khlebnikov@gmail.com>
2021-03-01 18:08:36 +0200

    6.14 Anecdotes and the backend, step2

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,13 +23,10 @@ export const initializeAnecdotes = (anecdotes) => {
   }
 }
 
-export const createAnecdote = (content) => {
+export const createAnecdote = (data) => {
   return {
     type: 'NEW_ANECDOTE',
-    data: {
-      content: content,
-      votes: 0
-    }
+    data: data
   }
 }
 
